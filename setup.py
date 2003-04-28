b006461004c8818992bf0b1d248f985b61e34841
davidb <davidb>
2003-04-28 16:46:31 +0000

    remove VRSN stuff

--- setup.py
+++ setup.py
@@ -4,11 +4,10 @@ from distutils.core import setup
 import os
 
 setup(name="python-rwhoisd",
-      version="0.1",
+      version="0.2",
       description="A lightweight RWhois server written in Python",
       author="David Blacka",
-      author_email="davidb@verisignlabs.com",
-      url="http://www.rwhois.net/",
+      author_email="david@blacka.com",
       packages=['rwhoisd'],
       scripts=['bin/'+x for x in os.listdir('bin')],
       data_files=[('lib/pyrwhoisd/sample_data',
