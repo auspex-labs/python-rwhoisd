b56e07b02c00b2d951d0f2de0d946be4844a763a
davidb <davidb>
2003-04-28 16:44:09 +0000

    copyright and license notices; added __len__ method to IndexResult

--- rwhoisd/MemDB.py
+++ rwhoisd/MemDB.py
@@ -1,3 +1,24 @@
+# This file is part of python-rwhoisd
+#
+# Copyright (C) 2003, David E. Blacka
+#
+# $Id: MemDB.py,v 1.3 2003/04/28 16:44:09 davidb Exp $
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+# USA
+
 import bisect, types
 import MemIndex, Cidr
 from Rwhois import rwhoisobject
@@ -239,6 +260,9 @@ class IndexResult:
         self.data = list
         self._dict = dict(zip(self.data, self.data))
 
+    def __len__(self):
+        return len(self.data)
+    
     def extend(self, list):
         if isinstance(list, type(self)):
             list = list.list()
