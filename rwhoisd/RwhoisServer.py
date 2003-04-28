6d8494f75d31678901433ae035ea9d41fea5a223
davidb <davidb>
2003-04-28 16:43:19 +0000

    copyright and license notices

--- rwhoisd/RwhoisServer.py
+++ rwhoisd/RwhoisServer.py
@@ -1,4 +1,23 @@
-#! /usr/bin/python
+# This file is part of python-rwhoisd
+#
+# Copyright (C) 2003, David E. Blacka
+#
+# $Id: RwhoisServer.py,v 1.3 2003/04/28 16:45:11 davidb Exp $
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+# USA
 
 import sys, socket, SocketServer
 
@@ -42,7 +61,6 @@ class RwhoisHandler(SocketServer.StreamRequestHandler):
         
     def handle(self):
 
-        print repr(self.request)
         self.quit_flag = False
 
         # output a banner
