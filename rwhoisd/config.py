c76fae7057c639603cf8afa5bd723cc8f5f29fdc
davidb <davidb>
2003-04-28 16:44:29 +0000

    copyright and license notices; version increase

--- rwhoisd/config.py
+++ rwhoisd/config.py
@@ -1,3 +1,24 @@
+# This file is part of python-rwhoisd
+#
+# Copyright (C) 2003, David E. Blacka
+#
+# $Id: config.py,v 1.3 2003/04/28 16:44:29 davidb Exp $
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+# USA
+
 """Server global variables should be set here."""
 
 import socket
@@ -27,7 +48,7 @@ verbose = False
 
 #### END Editable Configuration Options
 
-version = "0.1"
+version = "0.2"
 banner_string = "%%rwhois V-1.5 %s (python-rwhoisd %s)" % \
                 (server_hostname, version)
 
